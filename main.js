6f7bddd544a771dcf271f820d3768fcbc1fe16b3
Tom Amaral <txa2269@rit.edu>
2020-01-23 11:22:14 -0500

    added link to test html page

--- main.js
+++ main.js
@@ -6,9 +6,10 @@
 const express = require('express');
 const app = express();
 const port = 80;
+const path = require('path')
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname + '/www/test.html'));
 });
 
 app.listen(port);
