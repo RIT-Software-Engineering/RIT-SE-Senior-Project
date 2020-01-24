9099dfc720be7d385b1823af7d717efb0626bb94
Tom Amaral <txa2269@rit.edu>
2020-01-24 11:42:01 -0500

    added config for db handling

---
+++ server/database/db_config.js
@@ -0,0 +1,9 @@
+/**
+ * Responsible for maintaining variables and configurations for database management
+ */
+
+const path = require('path');
+
+ module.exports = {
+     dbFileName: 'senior_projects.db'
+ }
