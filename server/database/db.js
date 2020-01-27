5e566e4f8943296f0d078c7a12e1f99bf996362f
Tom Amaral <txa2269@rit.edu>
2020-01-27 13:07:20 -0500

    Added selectAll and Insert operations to db handler

--- server/database/db.js
+++ server/database/db.js
@@ -5,18 +5,47 @@ const PATH = require('path');
 const SQLITE3 = require('sqlite3').verbose();
 const CONFIG = require('./db_config');
 
-let seniorProjects;
+let seniorProjectsDB;
 
 /**
  * @function openReadWrite Opens the database in memory with read/write capabilities
  */
 function openReadWrite() {
-    seniorProjects = new SQLITE3.Database(PATH.join(__dirname, CONFIG.dbFileName), SQLITE3.OPEN_READWRITE, (err) => {
+    seniorProjectsDB = new SQLITE3.Database(PATH.join(__dirname, CONFIG.dbFileName), SQLITE3.OPEN_READWRITE, (err) => {
         if (err) {
             console.error(err.message);
         }
-        console.log('Connected to ' + CONFIG.dbFileName);
     });
-    
 }
 
+function selectAll() {
+    if (seniorProjectsDB) {
+        let sql = `SELECT * FROM senior_projects`;
+
+        seniorProjectsDB.all(sql, [], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            console.log(rows); // DEBUGGING
+        });
+    }
+}
+
+/**
+ * Takes an array of values and inserts them into a new row in the senior_projects table
+ * Array must be of size nine
+ * @param {Array} values 
+ */
+function insert(values) {
+    if (seniorProjectsDB) {
+        seniorProjectsDB.run(`INSERT INTO senior_projects (id, priority, title, members, sponsor, coach, synopsis, poster, website) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`, values);
+    }
+}
+let test1 = [1, 1, 'toms test', 'tom', 'also tom', 'its tom', 'toms test row', '/path/to/file', PATH.join(__dirname + '/img/test.jpg')];
+let test2 = [2, 1, 'toms test 2', 'tom', 'also tom', 'its tom', 'toms test row', '/path/to/file', PATH.join(__dirname + '/img/test.jpg')];
+
+//openReadWrite();
+insert();
+//selectAll();
+//seniorProjectsDB.close();
