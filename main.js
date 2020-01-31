66d29e805332c27db9ea66c6ad56371f0db82bf4
Tom Amaral <txa2269@rit.edu>
2020-01-31 10:47:23 -0500

    Added routing to www. Added support for requesting poster images

--- main.js
+++ main.js
@@ -7,9 +7,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path')
+const DB_CONFIG = require('./server/database/db_config');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/www/test.html'));
+let DBHandler = require('./server/database/db');
+
+// var handler = new DBHandler(DB_CONFIG.tableNames.senior_projects);
+// var test1 = [1, 1, "Tom's Test Row", 'Tom', 'S. Malachowsky', 'S. Malachowsky', 'A test row', 'test.jpg', 'www.google.com'];
+
+// handler.insert(test1);
+
+
+app.get('/db/selectAll', (req, res) => {
+    let handler = new DBHandler(DB_CONFIG.tableNames.senior_projects);
+    handler.selectAll().then(function(value) {
+        console.log(value);
+        res.send(value);
+    });
 });
 
+app.get('/db/getPoster', (req, res) => {
+    let screenedFileName = path.normalize(req.query.fileName).replace(/\\|\//g, ''); // attempt to avoid any path traversal issues
+
+    res.sendFile(path.join(__dirname, '/server/posters/' + screenedFileName));
+});
+
+app.use('/', express.static('./www'));
+app.use('/', express.static('./server/posters'));
 app.listen(port);
+
