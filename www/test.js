c84c4e992fe618d60a173fe2cd223621dd6e5e0f
Tom Amaral <txa2269@rit.edu>
2020-01-31 10:47:53 -0500

    sample code to show database and image support

--- www/test.js
+++ www/test.js
@@ -1,1 +1,16 @@
 console.log('hello world');
+
+let filename = 'test.jpg';
+
+$.get("/db/selectAll", function(data) {
+    console.log(data);
+    let result = document.createElement('p');
+    result.textContent = JSON.stringify(data);
+    document.body.appendChild(result);
+    //filename = data.poster;
+}).then(()=> {
+
+    let img = document.createElement('img');
+    img.src = '/db/getPoster?fileName=' + filename;
+    document.body.appendChild(img);
+});
