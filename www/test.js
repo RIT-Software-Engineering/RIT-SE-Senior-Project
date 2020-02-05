68af5b778b60c64ac499cfeb1e8c6c6603bbdc88
Tom Amaral <txa2269@rit.edu>
2020-02-05 12:07:12 -0500

    commented out example db call code

--- www/test.js
+++ www/test.js
@@ -1,16 +1,16 @@
 console.log('hello world');
 
-let filename = 'test.jpg';
+// let filename = 'test.jpg';
 
-$.get("/db/selectAll", function(data) {
-    console.log(data);
-    let result = document.createElement('p');
-    result.textContent = JSON.stringify(data);
-    document.body.appendChild(result);
-    //filename = data.poster;
-}).then(()=> {
+// $.get("/db/selectAll", function(data) {
+//     console.log(data);
+//     let result = document.createElement('p');
+//     result.textContent = JSON.stringify(data);
+//     document.body.appendChild(result);
+//     //filename = data.poster;
+// }).then(()=> {
 
-    let img = document.createElement('img');
-    img.src = '/db/getPoster?fileName=' + filename;
-    document.body.appendChild(img);
-});
+//     let img = document.createElement('img');
+//     img.src = '/db/getPoster?fileName=' + filename;
+//     document.body.appendChild(img);
+// });
