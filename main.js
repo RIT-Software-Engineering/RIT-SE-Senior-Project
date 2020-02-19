be1f8a6cfa7ddc4e3e300f50511cdf550c654c25
Tom Amaral <txa2269@rit.edu>
2020-02-19 12:15:31 -0500

    Cleaned up, added route skeletons for selectAll on specific tables

--- main.js
+++ main.js
@@ -8,30 +8,48 @@ const app = express();
 const port = 3000;
 const path = require('path')
 const DB_CONFIG = require('./server/database/db_config');
+const DBHandler = require('./server/database/db');
 
-let DBHandler = require('./server/database/db');
+let db = new DBHandler();
 
-// var handler = new DBHandler(DB_CONFIG.tableNames.senior_projects);
-// var test1 = [1, 1, "Tom's Test Row", 'Tom', 'S. Malachowsky', 'S. Malachowsky', 'A test row', 'test.jpg', 'www.google.com'];
+//#region 'Select All' Routes
 
-// handler.insert(test1);
-let handler = new DBHandler(DB_CONFIG.tableNames.senior_projects);
-
-app.get('/db/selectAll', (req, res) => {
-    handler.selectAll().then(function(value) {
+app.get('/db/selectAllSponsorInfo', (req, res) => {
+    db.selectAll(DB_CONFIG.tableNames.sponsor_info).then(function(value) {
         console.log(value);
         res.send(value);
     });
+});
+
+app.get('/db/selectAllStudentInfo', (req, res) => {
+    res.status(404).send('Sorry, route not yet available');
+    /*
+    db.selectAll(DB_CONFIG.tableNames.student_info).then(function(value) {
+        console.log(value);
+        res.send(value);
+    });
+    */
+});
+
+app.get('/db/selectAllCoachInfo', (req, res) => {
+    res.status(404).send('Sorry, route not yet available');
+    /*
+    db.selectAll(DB_CONFIG.tableNames.coach_info).then(function(value) {
+        console.log(value);
+        res.send(value);
+    });
+    */
 });
 
 app.get('/db/selectExemplary', (req, res) => {
     let sql = 'SELECT * FROM ' + DB_CONFIG.tableNames.senior_projects + ' WHERE priority = ?';
     let values = [0];
 
-    handler.select(sql, values).then(function(value) {
+    db.select(sql, values).then(function(value) {
         res.send(value);
     });
 });
+//#endregion
 
 app.get('/db/getPoster', (req, res) => {
     let screenedFileName = path.normalize(req.query.fileName).replace(/\\|\//g, ''); // attempt to avoid any path traversal issues
@@ -40,6 +58,6 @@ app.get('/db/getPoster', (req, res) => {
 });
 
 app.use('/', express.static('./www'));
+app.use('/', express.static('./www/admin'));
 app.use('/', express.static('./server/posters'));
 app.listen(port);
-
