8c01d1470a8fde01d1d6f26f3b96fddb5dcbc893
Tom Amaral <txa2269@rit.edu>
2020-02-19 10:42:58 -0500

    Table schema for sponsor info

---
+++ server/database/sponsor_info_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE sponsor_info (
+    id          INTEGER PRIMARY KEY NOT NULL,
+    company     TEXT,
+    division    TEXT,
+    type        TEXT,
+    fname       TEXT,
+    lname       TEXT,
+    email       TEXT,
+    phone       TEXT,
+    association TEXT,
+    notes       TEXT
+);
