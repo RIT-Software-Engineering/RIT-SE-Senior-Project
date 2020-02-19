bf9459322ab56ce7a3401d69ea1e38449d1d459f
Tom Amaral <txa2269@rit.edu>
2020-02-19 12:37:40 -0500

    test info insertion for sponsor_info

---
+++ server/database/test data/sponsor_test_info.sql
@@ -0,0 +1,6 @@
+INSERT INTO sponsor_info (company, division, type, fname, lname, email, phone, association, notes)
+VALUES 
+    ("Test Company One", "Testing", "Internal", "Joe", "Smith", "joe.smith@email.com", "+1 585 555 5555", "None", "Met at a testing conference"),
+    ("Test Company Two", "Testing", "Internal", "Tim", "Dyer", "tim.dyer@email.com", "+1 585 555 5555", "None", "Met at a testing conference"),
+    ("Test Company Three", "Testing", "Internal", "Dork", "Smith", "dork.smith@email.com", "+1 585 555 5555", "None", "Joe Smith's brother, from Test Company One")
+    ;
