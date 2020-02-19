724f149e67c80a99bd1e1673bfd7890487980272
Tom Amaral <txa2269@rit.edu>
2020-02-19 10:43:18 -0500

    removed not null requirements in preparation for new schema

--- server/database/senior_projects_table.sql
+++ server/database/senior_projects_table.sql
@@ -1,13 +1,13 @@
 CREATE TABLE senior_projects (
     id          int primary key unique not null,
-    priority    int not null,
-    title       text not null,
-    teamName    text not null,
-    members     text not null,
-    sponsor     text not null,
-    coach       text not null,
-    poster      text not null,
+    priority    int,
+    title       text,
+    teamName    text,
+    members     text,
+    sponsor     text,
+    coach       text,
+    poster      text,
     video       text,
-    website     text not null,
-    synopsis    text not null
+    website     text,
+    synopsis    text
 );
