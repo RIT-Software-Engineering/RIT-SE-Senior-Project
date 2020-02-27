ace29602a032007984a3d6301493c96872a0ac03
Tom Amaral <txa2269@rit.edu>
2020-02-27 15:05:49 -0500

    Clientside form validation and window reload forcing

--- www/sponsor/sponsor.js
+++ www/sponsor/sponsor.js
@@ -1,9 +1,15 @@
+
+window.onunload = function(){};
+
 $('.ui.accordion').accordion();
 
 $('.ui.checkbox').checkbox();
 
 $('.ui.modal').modal({
-    closable: false
+    closable: false,
+    onShow: function() {
+        $('.ui.form').form('clear');
+    }
 });
 
 $('#proposalBtn').on('click', function(data) {
@@ -15,6 +21,13 @@ $('#proposalBtn').on('click', function(data) {
 $('.ui.form').form({
 
     fields: {
-        
+        title                   :   'empty',
+        organization            :   'empty',
+        primary_contact         :   'empty',
+        contact_email           :   'empty',
+        contact_phone           :   'empty',
+        sponsor_avail_checked   :   'checked',
+        project_agreements_checked :'checked',
+        rights                  :   'checked'
     }
 });
