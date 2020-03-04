e6ec8a72d31667b1fdffa4dd9be5b4f6e8487e1b
Tom Amaral <txa2269@rit.edu>
2020-03-04 11:35:59 -0500

    added a list of proposal form value keys to make iteration easier

--- server/database/db_config.js
+++ server/database/db_config.js
@@ -13,5 +13,25 @@
         sponsor_info:       'sponsor_info',
         student_info:       'student_info',
         coach_info:         'coach_info'
-    }
+    },
+    senior_project_proposal_keys : [
+        'title', 
+        'organization', 
+        'primary_contact', 
+        'contact_email', 
+        'contact_phone',
+        'background_info', 
+        'project_description', 
+        'project_scope', 
+        'project_challenges',
+        'sponsor_provided_resources', 
+        'constraints_assumptions', 
+        'sponsor_deliverables',
+        'proprietary_info', 
+        'sponsor_alternate_time', 
+        'sponsor_avail_checked', 
+        'project_agreements_checked',
+        'assignment_of_rights'
+    ]
+
  }
