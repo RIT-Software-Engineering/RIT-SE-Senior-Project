c2ad3d49203bc5452db8d174363729ae70e2a51f
Tom Amaral <txa2269@rit.edu>
2020-04-03 11:10:15 -0400

    Added global config file with supported file types for file submissions

---
+++ server/config.js
@@ -0,0 +1,18 @@
+/**
+ * A place for global variables to be configured and made available
+ */
+module.exports = {
+    accepted_file_types: [
+        '.pdf',
+        '.jpeg',
+        '.jpg',
+        '.png',
+        '.doc',
+        '.docx',
+        '.xls',
+        '.xlsx',
+        '.ppt',
+        '.pptx'
+    ]
+
+}
