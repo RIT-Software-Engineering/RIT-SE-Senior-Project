168334e7bb61c10344e2eaa5cef239fc1b594cb2
Tom Amaral <txa2269@rit.edu>
2020-04-08 11:02:41 -0400

    moved auth function to config

--- server/routing/index.js
+++ server/routing/index.js
@@ -6,7 +6,6 @@
 
 // Imports
 const router = require('express').Router();
-const auth = require('basic-auth');
 const path = require('path');
 const CONFIG = require('../config');
 const db_router = require('./db_routes');
@@ -14,31 +13,12 @@ const db_router = require('./db_routes');
 
 
 
-
-let authAdmin = function(req, res, next) {
-    let user = auth(req);
-    if (!user || !user.name || !user.pass) {
-        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-        res.sendStatus(401);
-        return;
-    }
-    if (user.name === 'M' && user.pass === '123') {
-        next();
-    } else {
-        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-        res.sendStatus(401);
-        return;
-    }
-}
-
-
 //#region Page routes
 router.get('/', (req, res) => {
     res.sendFile(path.join(CONFIG.www_path, '/html/index.html'));
 });
 
-router.get('/admin', authAdmin, (req, res) => {
-    router.use(express.static('../proposal_docs'));
+router.get('/admin', CONFIG.authAdmin, (req, res) => {
     res.sendFile(path.join(CONFIG.www_path, '/html/admin.html'));
 });
 
