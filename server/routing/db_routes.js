feb28ca264b6e5aec7b25f1d3cd46548973a57cd
Tom Amaral <txa2269@rit.edu>
2020-04-20 09:51:27 -0400

    stub for route to send all team timelines to the client. doesnt work

--- server/routing/db_routes.js
+++ server/routing/db_routes.js
@@ -231,4 +231,53 @@ db_router.get('/getPoster', (req, res) => {
 });
 
 
+db_router.get('/getAllTeamTimelines', authAdmin, (req, res) => {
+    // Needs better name, references the idea of 'Semester groups'
+    // i.e. Fall 2019 - Spring 2020
+    let block = {
+        name: 'Fall 2019 - Spring 2020', // some plain text readable name
+        status : 'future / active / archived',
+        attributes : 'table reference'
+    }
+
+    // Sub table belonging to a semester
+    let attribute = {
+        dates : {
+            posterDueDate : ''
+        },
+
+        long_description : '',
+        short_description : ''
+    }
+
+    
+    let taskObjects = [
+        {
+            name : 'task3',
+            text : 'test text',
+            state : 'green',
+            studentAction: '',
+            coachAction: ''
+        },
+        {
+            name : 'task4',
+            text : 'test text',
+            state : 'red'
+        },
+        {
+            name : 'task5',
+            text : 'test text',
+            state : 'yellow'
+        },
+        {
+            name : 'task6',
+            text : 'test text',
+            state : 'grey'
+        }
+    ]
+
+
+    res.send(payload);
+}); 
+
 module.exports = db_router;
