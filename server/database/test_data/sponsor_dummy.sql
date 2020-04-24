06bde0eb9a3e981a5d0b10716d8a0ffec6ade5cc
Tom Amaral <txa2269@rit.edu>
2020-04-24 11:01:01 -0400

    Renamed, added 2 more sponsors to sponsor dummy

--- server/database/test_data/sponsor_dummy.sql
+++ server/database/test_data/sponsor_dummy.sql
@@ -1,5 +1,7 @@
-INSERT INTO sponsor_info (fname, lname, company, division, email, phone)
+INSERT INTO sponsors (fname, lname, company, division, email, phone)
 VALUES 
+    ("Progressor", "Man", "Progress LLC", NULL, "progressorman@progress.com", "+1 555 555 5454"),
+    ("Completer", "Person", "Completion Solutions", NULL, "complter@completion.com", "+1 565 565 5656")
     ("Tom", "Test", "Test Company", "Testing Division", "tom.test@company.com", "+1 555 555 5555"),
     ("Joe", "Smith", "Apple Inc", "Software Dev", "joe.smith@company.com", "+1 555 555 6666"),
     ("Phil", "Martin", "McDonalds", "Marketing", "phil.martin@company.com", "+1 555 777 7777")
