e6085b0242c36bcf514e814189c64d66f7068061
Tom Amaral <txa2269@rit.edu>
2020-04-27 20:35:30 -0400

    Some basic user dummy data, student, admin, and coach

---
+++ server/database/test_data/user_dummy.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (system_id, fname, lname, email, type, semester_group, project) 
+VALUES
+    ("txa2269", "Tom", "Amaral", "txa2269@rit.edu", "student", 1, 3),
+    ("abc123", "John", "Smith", "abc123@rit.edu", "student", 1, 3),
+    ("cba123", "Jack", "James", "cba123@rit.edu", "student", 1, 3),
+    ("xyz123", "Jack", "James", "xyz123@rit.edu", "student", 1, 3),
+    ("samvse", "Samuel", "Malachowsky", "samvse@rit.edu", "admin", 1, 3),
+    ("llkiee", "Larry", "Kiser", "llkiee@rit.edu", "coach", 1, 3)
+    ;
