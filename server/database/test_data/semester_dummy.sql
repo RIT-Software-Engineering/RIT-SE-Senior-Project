5777d9498e25b40484875771288f50efd50aeeb5
Tom Amaral <txa2269@rit.edu>
2020-04-27 20:35:04 -0400

    Semester group dummy data

---
+++ server/database/test_data/semester_dummy.sql
@@ -0,0 +1,7 @@
+INSERT INTO semester_group (name, dept, start_date, end_date)
+VALUES
+
+    ("2020-20 Spring / Summer", "SE", "1/13/2020", "8/7/2020"),
+    ("2019-20 Fall / Spring", "SE", "8/27/2019", "5/8/2020"),
+    ("2019-19 Spring / Summer", "SE", "1/13/2019", "8/7/2019")
+    ;
