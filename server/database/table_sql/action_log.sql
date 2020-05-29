ff4cbae805a13b391d5c97c68d23f9f98069ac1c
Tom Amaral <txa2269@rit.edu>
2020-05-29 08:49:06 -0400

    updated to conform to sqlite foreign key syntax

--- server/database/table_sql/action_log.sql
+++ server/database/table_sql/action_log.sql
@@ -1,7 +1,13 @@
+DROP TABLE action_log;
+
 CREATE TABLE action_log (
-    FOREIGN KEY (action_id) REFERENCES actions(action_id),
+    action_id       INTEGER PRIMARY KEY AUTOINCREMENT, -- internal id for this row
+    action_template INTEGER NOT NULL,                  -- the id of the template action this is formatted on
+    system_id       INTEGER NOT NULL,                  -- system ID of executor 
+    project         INTEGER NOT NULL,               
+    form_data       BLOB,
+    files           BLOB,
+    FOREIGN KEY (action_template) REFERENCES actions(action_id),
     FOREIGN KEY (system_id) REFERENCES users(system_id),
-    FOREIGN KEY (project) REFERENCES projects(project_id),
-    form_data   BLOB,
-    files       BLOB    
+    FOREIGN KEY (project) REFERENCES projects(project_id)    
 );
