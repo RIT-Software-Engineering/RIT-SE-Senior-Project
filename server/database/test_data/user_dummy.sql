44694c5c708228b6cb53b45973c094ec3866b363
Tom Amaral <txa2269@rit.edu>
2020-06-02 10:55:12 -0400

    added another team

--- server/database/test_data/user_dummy.sql
+++ server/database/test_data/user_dummy.sql
@@ -1,9 +1,14 @@
 INSERT INTO users (system_id, fname, lname, email, type, semester_group, project) 
 VALUES
-    ("txa2269", "Tom", "Amaral", "txa2269@rit.edu", "student", 1, 3),
+    ("dxb2269", "Dude", "Bro", "dxb2269@rit.edu", "student", 1, 3),
     ("abc123", "John", "Smith", "abc123@rit.edu", "student", 1, 3),
     ("cba123", "Jack", "James", "cba123@rit.edu", "student", 1, 3),
     ("xyz123", "Jack", "James", "xyz123@rit.edu", "student", 1, 3),
+    ("txa2269", "Tom", "Amaral", "txa2268@rit.edu", "student", 2, 4),
+    ("srs1234", "Shawn", "Struble", "srs1234@rit.edu", "student", 2, 4),
+    ("cxw1234", "Calvin", "Wu", "cxw1234@rit.edu", "student", 2, 4),
+    ("pxk1234", "Nick", "Kytcuaktaselt", "pxk1234@rit.edu", "student", 2, 4),
     ("samvse", "Samuel", "Malachowsky", "samvse@rit.edu", "admin", NULL, NULL),
-    ("llkiee", "Larry", "Kiser", "llkiee@rit.edu", "coach", NULL, NULL)
+    ("llkiee", "Larry", "Kiser", "llkiee@rit.edu", "coach", NULL, NULL),
+    ("dxkvse", "Scott", "Hawker", "dxkvse@rit.edu", "coach", NULL, NULL)
     ;
