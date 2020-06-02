97e42df4636404cc05cabf68766fab513f53bdd1
Tom Amaral <txa2269@rit.edu>
2020-06-02 10:54:12 -0400

    added more test actions

--- server/database/test_data/actions_dummy.sql
+++ server/database/test_data/actions_dummy.sql
@@ -17,8 +17,8 @@ VALUES
         0,
         "This is the first test form for individuals",
         "individual",
-        "2020-8-7",
-        "2020-8-10",
+        "2019-8-27",
+        "2019-9-7",
         "
         <h1>Take the individual test form</h1>
         <form>
@@ -34,6 +34,50 @@ VALUES
         0, 
         "Turn in your <b>interim</b> report",
          "team", 
+        "2019-9-13",
+        "2019-9-30",
+        "<h1> Submit your interim report </h1>
+        <form>
+            <input type=""text""/>
+        </form>
+        "
+
+    
+    ),
+    (
+        1, "Team Name Submission", 0, "Come up <u>with</u> team name", 
+        "team",
+        "2019-10-13",
+        "2019-10-30",
+        "<h1> Submit a team name </h1>
+        <form>
+            <input type=""text""/>
+        </form>
+        "
+    ),
+    (
+        2,
+        "Individual Test Form 1",
+        0,
+        "This is the first test form for individuals",
+        "individual",
+        "2020-8-7",
+        "2020-8-10",
+        "
+        <h1>Take the individual test form</h1>
+        <form>
+            <label>Test question 1</label>
+            <input name=""question1"" type=""text""/>
+            <input type=""submit""/>
+        </form>
+        "
+    ),
+    (
+        2, 
+        "Report Submission", 
+        0, 
+        "Turn in your <b>interim</b> report",
+         "team", 
         "2020-1-13",
         "2020-1-30",
         "<h1> Submit your interim report </h1>
@@ -45,7 +89,7 @@ VALUES
     
     ),
     (
-        1, "Team Name Submission", 0, "Come up <u>with</u> team name", 
+        2, "Team Name Submission", 0, "Come up <u>with</u> team name", 
         "team",
         "2020-1-13",
         "2020-1-30",
