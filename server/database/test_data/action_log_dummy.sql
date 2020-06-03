d2a038f551550178fb606495f5201b4bd66d46d4
Tom Amaral <txa2269@rit.edu>
2020-06-03 11:25:46 -0400

    added dummy action log items

---
+++ server/database/test_data/action_log_dummy.sql
@@ -0,0 +1,11 @@
+INSERT INTO action_log(
+    action_template,
+    system_id,
+    project,
+    form_data,
+    files
+)
+VALUES 
+    (3, "dxb2269", 3, "{""team_name"": ""The Null Pointers""}", null),
+    (6, "txa2269", 4, "{""team_name"": ""Drop Table Admin""}", null)
+;
