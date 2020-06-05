f6aeae487db2d1b923e74c40d9eef25db1f7a82c
Tom Amaral <txa2269@rit.edu>
2020-06-05 08:37:56 -0400

    added four test rows for an individual task

--- server/database/test_data/action_log_dummy.sql
+++ server/database/test_data/action_log_dummy.sql
@@ -7,5 +7,9 @@ INSERT INTO action_log(
 )
 VALUES 
     (3, "dxb2269", 3, "{""team_name"": ""The Null Pointers""}", null),
-    (6, "txa2269", 4, "{""team_name"": ""Drop Table Admin""}", null)
+    (6, "txa2269", 4, "{""team_name"": ""Drop Table Admin""}", null),
+    (1, "dxb2269", 3, "{""name"": ""Dude""}", null),
+    (1, "abc123", 3, "{""name"": ""John""}", null),
+    (1, "cba123", 3, "{""name"": ""Jack""}", null),
+    (1, "xyz123", 3, "{""name"": ""Gal""}", null)
 ;
