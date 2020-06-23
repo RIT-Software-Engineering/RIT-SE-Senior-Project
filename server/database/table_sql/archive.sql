17f53260bd205f8ae794167342884bcb5de68e13
Tom Amaral <txa2269@rit.edu>
2020-06-23 10:19:15 -0400

    comments

--- server/database/table_sql/archive.sql
+++ server/database/table_sql/archive.sql
@@ -1,15 +1,15 @@
 CREATE TABLE archive (
     archive_id      INTEGER PRIMARY KEY AUTOINCREMENT,
     project_id      INTEGER,
-    priority        INTEGER, 
+    priority        INTEGER, -- how high a project should be displayed on clientside
     title           TEXT, 
     team_name       TEXT,      
     members         TEXT, 
     sponsor         TEXT, 
     coach           TEXT, 
-    poster_thumb    TEXT,
-    poster_full     TEXT, 
+    poster_thumb    TEXT,   -- path to poster thumbnail
+    poster_full     TEXT,   -- path to full resolution poster image 
     synopsis        TEXT,
-    video           TEXT,
+    video           TEXT,   -- path to project video (if any)
     FOREIGN KEY (project_id) REFERENCES projects(project_id)
 );
