3b9b04f9d8908a60407f5b9f703f84e4639653fc
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2021-01-04 20:53:36 -0500

    Small cleanup - Inital proposal form state is not needed

--- ui/src/components/pages/ProposalPage.js
+++ ui/src/components/pages/ProposalPage.js
@@ -2,25 +2,7 @@ import React, {useState} from 'react';
 
 function ProposalPage() {
 
-    const [formData, setActualFormData] = useState({
-            title: "",
-            organization: "",
-            primary_contact: "",
-            contact_email: "",
-            contact_phone: "",
-            background_info: "",
-            project_description: "",
-            project_scope: "",
-            project_challenges: "",
-            sponsor_provided_resources: "",
-            constraints_assumptions: "",
-            sponsor_deliverables: "",
-            proprietary_info: "",
-            sponsor_alternate_time: "",
-            sponsor_avail_checked: "",
-            project_agreements_checked: "",
-            assignment_of_rights: "",
-    })
+    const [formData, setActualFormData] = useState({})
 
     const setFormData = (event) => {
         const target = event.target;
