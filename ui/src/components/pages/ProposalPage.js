4862b0657fc36d2a7823ff5834eb3d2e278a0e58
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2021-01-09 21:18:24 -0500

    Convert more of the proposal form to React

--- ui/src/components/pages/ProposalPage.js
+++ ui/src/components/pages/ProposalPage.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { useHistory } from 'react-router-dom';
-import { Modal, Form, Radio } from 'semantic-ui-react'
+import { Modal, Form, Radio, Divider } from 'semantic-ui-react'
 import Header from './../shared/Header';
 import Footer from './../shared/Footer';
 
@@ -47,8 +47,13 @@ function ProposalPage() {
         setFormData({target: {type: 'radio', value: value, name: name}})
     }
 
-    const submitMockProposal = async (event) => {
+    const submitProposal = async (event) => {
         event.preventDefault();
+
+        if(modalOpen) {
+            console.warn("Trying to submit proposal form while modal is open.");
+            return;
+        }
 
         const body = new FormData();
         Object.keys(formData).forEach((key) => {
@@ -110,8 +115,6 @@ function ProposalPage() {
         }
     }
 
-    console.log("formData.sponsor_avail_checked", formData.sponsor_avail_checked);
-
     return (
         <div id="page">
             <Modal
@@ -129,31 +132,13 @@ function ProposalPage() {
                 <div className="row">
                     <h2>Submit A Project Proposal</h2>
                 </div>
-                <Form id="proposalForm" className="ui form" onSubmit={(e) => {submitMockProposal(e)}} >
-                    <Form.Field>
-                        <label>Project Title</label>
-                        <input name="title" value={formData.title || ""} type="text" onChange={(e)=>{setFormData(e)}} />
-                    </Form.Field>
-
-                    <Form.Field>
-                        <label>Organization Name</label>
-                        <input name="organization" value={formData.organization || ""} type="text" onChange={(e)=>{setFormData(e)}} />
-                    </Form.Field>
-
-                    <Form.Field>
-                        <label>Primary Contact Name</label>
-                        <input name="primary_contact" value={formData.primary_contact || ""} type="text" onChange={(e)=>{setFormData(e)}} />
-                    </Form.Field>
+                <Form id="proposalForm" className="ui form" onSubmit={(e) => {submitProposal(e)}} >
+                    <Form.Input label="Project Title" name="title" value={formData.title || ""} onChange={(e)=>{setFormData(e)}} />
+                    <Form.Input label="Organization Name" name="organization" value={formData.organization || ""} onChange={(e)=>{setFormData(e)}} />
+                    <Form.Input label="Primary Contact Name" name="primary_contact" value={formData.primary_contact || ""} onChange={(e)=>{setFormData(e)}} />
                     <div className="two fields">
-                        <Form.Field>
-                            <label>Email</label>
-                            <input name="contact_email" value={formData.contact_email || ""} type="text" onChange={(e)=>{setFormData(e)}} />
-                        </Form.Field>
-
-                        <Form.Field>
-                            <label>Phone</label>
-                            <input name="contact_phone" value={formData.contact_phone || ""} type="text" onChange={(e)=>{setFormData(e)}} />
-                        </Form.Field>
+                        <Form.Input label="Email" name="contact_email" value={formData.contact_email || ""} onChange={(e)=>{setFormData(e)}} />
+                        <Form.Input label="Phone" name="contact_phone" value={formData.contact_phone || ""} onChange={(e)=>{setFormData(e)}} />
                     </div>
 
                     <Form.Field>
@@ -161,53 +146,17 @@ function ProposalPage() {
                         <input name="attachments" value={formData.attachments || ""} type="file" accept=".pdf, .png, .jpg, .jpeg" multiple onChange={(e)=>{setFormData(e)}} />
                     </Form.Field>
 
-                    <Form.Field>
-                        <label>Project Background Information</label>
-                        <textarea name="background_info" value={formData.background_info || ""} onChange={(e)=>{setFormData(e)}} ></textarea>
-                    </Form.Field>
-                    
-                    <Form.Field>
-                        <label>Project Description</label>
-                        <textarea name="project_description" value={formData.project_description || ""} onChange={(e)=>{setFormData(e)}} ></textarea>
-                    </Form.Field>
+                    <Form.TextArea label="Project Background Information" name="background_info" value={formData.background_info || ""} onChange={(e)=>{setFormData(e)}} ></Form.TextArea>
+                    <Form.TextArea label="Project Description" name="project_description" value={formData.project_description || ""} onChange={(e)=>{setFormData(e)}} ></Form.TextArea>
+                    <Form.TextArea label="Project Scope" name="project_scope" value={formData.project_scope || ""} onChange={(e)=>{setFormData(e)}} ></Form.TextArea>
+                    <Form.TextArea label="Project Challenges" name="project_challenges" value={formData.project_challenges || ""} onChange={(e)=>{setFormData(e)}} ></Form.TextArea>
+                    <Form.TextArea label="Constraints & Assumptions" name="constraints_assumptions" value={formData.constraints_assumptions || ""} onChange={(e)=>{setFormData(e)}} ></Form.TextArea>
+                    <Form.TextArea label="Sponsor-Provided Resources" name="sponsor_provided_resources" value={formData.sponsor_provided_resources || ""} onChange={(e)=>{setFormData(e)}} ></Form.TextArea>
+                    <Form.Input label="Project Search Keywords" name="project_search_keywords" value={formData.project_search_keywords || ""} onChange={(e)=>{setFormData(e)}} />
+                    <Form.TextArea label="Sponsor and Project Specific Deliverables" name="sponsor_deliverables" value={formData.sponsor_deliverables || ""} onChange={(e)=>{setFormData(e)}} ></Form.TextArea>
+                    <Form.TextArea label="Proprietary Information" name="proprietary_info" value={formData.proprietary_info || ""} onChange={(e)=>{setFormData(e)}} ></Form.TextArea>
 
-                    <Form.Field>
-                        <label>Project Scope</label>
-                        <textarea name="project_scope" value={formData.project_scope || ""} onChange={(e)=>{setFormData(e)}} ></textarea>
-                    </Form.Field>
-
-                    <Form.Field>
-                        <label>Project Challenges</label>
-                        <textarea name="project_challenges" value={formData.project_challenges || ""} onChange={(e)=>{setFormData(e)}} ></textarea>
-                    </Form.Field>
-
-                    <Form.Field>
-                        <label>Constraints & Assumptions</label>
-                        <textarea name="constraints_assumptions" value={formData.constraints_assumptions || ""} onChange={(e)=>{setFormData(e)}} ></textarea>
-                    </Form.Field>
-
-                    <Form.Field>
-                        <label>Sponsor-Provided Resources</label>
-                        <textarea name="sponsor_provided_resources" value={formData.sponsor_provided_resources || ""} onChange={(e)=>{setFormData(e)}} ></textarea>
-                    </Form.Field>
-
-                    <Form.Field>
-                        <label>Project Search Keywords</label>
-                        <input name="project_search_keywords" value={formData.project_search_keywords || ""} type="text" onChange={(e)=>{setFormData(e)}} />
-                    </Form.Field>
-
-                    <Form.Field>
-                        <label>Sponsor and Project Specific Deliverables</label>
-                        <textarea name="sponsor_deliverables" value={formData.sponsor_deliverables || ""} onChange={(e)=>{setFormData(e)}} ></textarea>
-                    </Form.Field>
-
-                    <Form.Field>
-                        <label>Proprietary Information</label>
-                        <textarea name="proprietary_info" value={formData.proprietary_info || ""} onChange={(e)=>{setFormData(e)}} ></textarea>
-                    </Form.Field>
-
-                    <br />
-                    <div className="ui divider"></div>
+                    <Divider section/>
 
                     <h3>Sponsor Availability</h3>
                     <p>Sponsor personnel will be available to meet with the team once per week during the time set for meeting with the sponsor which is 
@@ -221,13 +170,9 @@ function ProposalPage() {
                             <label>I agree</label>
                         </div>
                     </Form.Field>
-                    <Form.Field>
-                        <label>If you will not be available during the standard senior project meeting time above, please give your timing constraints.</label>
-                        <input name="sponsor_alternate_time" value={formData.sponsor_alternate_time || ""} type="text" onChange={(e)=>{setFormData(e)}} />
-                    </Form.Field>
+                    <Form.Input label="If you will not be available during the standard senior project meeting time above, please give your timing constraints." name="sponsor_alternate_time" value={formData.sponsor_alternate_time || ""} onChange={(e)=>{setFormData(e)}} />
 
-                    <br />
-                    <div className="ui divider"></div>
+                    <Divider section/>
 
                     <h3>Project Agreements and Assignment of Rights</h3>
                     <p>RIT policy gives students full ownership of any work done as part of coursework which includes their work on senior project. 
@@ -242,10 +187,10 @@ function ProposalPage() {
                         box below.
                     </p>
                     <h4>Corporate and Legal Clearance of Project Agreement</h4>
-                            <p>
-                                We have the necessary corporate or legal clearances to use the unmodified project agreement. 
-                                (Note: The project agreements are cleared for RIT internal projects.)
-                            </p>
+                        <p>
+                            We have the necessary corporate or legal clearances to use the unmodified project agreement. 
+                            (Note: The project agreements are cleared for RIT internal projects.)
+                        </p>
                     <Form.Field>
                         <div className="ui checkbox">
                             <input name="project_agreements_checked" checked={formData.project_agreements_checked || false} type="checkbox" tabIndex="0" onChange={(e)=>{setFormData(e)}}/>
@@ -280,7 +225,7 @@ function ProposalPage() {
                                 </p>
                             </div>
                         </Form.Field>
-                        <div className="ui hidden divider"></div>
+                        <Divider hidden/>
                         <Form.Field>
                             <div className="ui radio checkbox">
                                 <Radio
@@ -304,7 +249,7 @@ function ProposalPage() {
                                 </p>
                             </div>
                         </Form.Field>
-                        <div className="ui hidden divider"></div>
+                        <Divider hidden/>
                         <Form.Field>
                         <div className="ui radio checkbox">
                             <Radio
