dd3bd7dcf9999905340bfc5fc00485c38f7be3a3
prettier <prettier>
2021-01-24 18:55:48 -0500

    prettier + delete unused www folder

--- server/main.js
+++ server/main.js
@@ -1,17 +1,17 @@
 /**
  * Main entry point for the SE Senior Project web server
  * @author Tom Amaral <txa2269@rit.edu>
-*/
+ */
 
-'use strict';
+"use strict";
 
 // Imports
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const bodyParser = require('body-parser');
-const fileupload = require('express-fileupload');
-const routing = require('./server/routing/index');
+const bodyParser = require("body-parser");
+const fileupload = require("express-fileupload");
+const routing = require("./server/routing/index");
 
 // Constants
 const port = 3001;
@@ -19,10 +19,12 @@ const port = 3001;
 // Set up body parsing and file upload configurations
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(fileupload({ 
-    safeFileNames: true, 
-    preserveExtension: true
-}));
+app.use(
+    fileupload({
+        safeFileNames: true,
+        preserveExtension: true,
+    })
+);
 
 // Setup CORS policies
 // TODO-IMPORTANT: LOOK FOR BEST PRACTICE CORS POLICIES
@@ -32,16 +34,18 @@ app.use(fileupload({
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 // });
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
 
 // Attach route handlers
-app.use('/', routing);
+app.use("/", routing);
 
 // Expose js and css as public resources
-app.use(express.static('./www/script'));
-app.use(express.static('./www/style'));
-app.use(express.static('./www/doc'));
+app.use(express.static("./www/script"));
+app.use(express.static("./www/style"));
+app.use(express.static("./www/doc"));
 
 app.listen(port);
--- server/server/routing/index.js
+++ server/server/routing/index.js
@@ -1,38 +1,35 @@
 /**
  * Index of routing.
-*/
+ */
 
-'use strict';
+"use strict";
 
 // Imports
-const router = require('express').Router();
-const path = require('path');
-const CONFIG = require('../config');
-const db_router = require('./db_routes');
-
-
-
+const router = require("express").Router();
+const path = require("path");
+const CONFIG = require("../config");
+const db_router = require("./db_routes");
 
 //#region Page routes
-router.get('/', (req, res) => {
-    res.sendFile(path.join(CONFIG.www_path, '/html/index.html'));
+router.get("/", (req, res) => {
+    res.sendFile(path.join(CONFIG.www_path, "/html/index.html"));
 });
 
-router.get('/admin', CONFIG.authAdmin, (req, res) => {
-    res.sendFile(path.join(CONFIG.www_path, '/html/admin.html'));
+router.get("/admin", CONFIG.authAdmin, (req, res) => {
+    res.sendFile(path.join(CONFIG.www_path, "/html/admin.html"));
 });
 
-router.get('/sponsor', (req, res) => {
-    res.sendFile(path.join(CONFIG.www_path, '/html/sponsor.html'));
+router.get("/sponsor", (req, res) => {
+    res.sendFile(path.join(CONFIG.www_path, "/html/sponsor.html"));
 });
 
-router.get('/proposalForm', (req, res) => {
-    res.sendFile(path.join(CONFIG.www_path, '/html/proposalForm.html'));
+router.get("/proposalForm", (req, res) => {
+    res.sendFile(path.join(CONFIG.www_path, "/html/proposalForm.html"));
 });
 
 //#endregion
 
 // Database routes
-router.use('/db', db_router);
+router.use("/db", db_router);
 
 module.exports = router;
