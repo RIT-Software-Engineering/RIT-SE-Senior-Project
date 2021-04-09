d3c7b41b05f73b2b7d56d0b01234b99df4c26ff2
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2021-04-09 01:23:08 -0400

    Make scripts executable + add to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-echo "Updaint code..."
+echo "Updating code..."
 git pull
 echo "Moving nginx config to ~/../../etc/nginx/conf.d/"
-cp ~/RIT-SE-Senior-Project/nginx/senior-project.conf ~/../../etc/nginx/conf.d/
+sudo cp ~/RIT-SE-Senior-Project/nginx/senior-project.conf ~/../../etc/nginx/conf.d/
+echo "Installing dependencies"
+cd ./server
+npm install
+cd ../ui
+npm install
+echo "Building react app"
+npm build
+cd ..
+echo "Stopping nginx"
+sudo systemctl stop nginx
+echo "Starting nginx"
+sudo systemctl start nginx
+echo "Stopping pm2"
+pm2 stop main
+echo "Starting pm2"
+pm2 start ./server/main.js
