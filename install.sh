42869bf2fc09881a279ea08efc13d20217582869
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2021-05-24 14:02:43 -0400

    Update deployment script

--- install.sh
+++ install.sh
@@ -2,18 +2,17 @@
 
 echo "Updating code..."
 git pull
-echo "Moving nginx config to ~/../../etc/nginx/conf.d/"
-sudo cp ~/../website/RIT-SE-Senior-Project/nginx/senior-project.conf ~/../../etc/nginx/conf.d/
-echo "Moving tls_params config to ~/../../etc/nginx/conf.d/"
-sudo cp ~/../website/RIT-SE-Senior-Project/nginx/tls_params ~/../../etc/nginx/
+echo "Moving nginx config to /etc/nginx/conf.d/"
+sudo cp /home/website/website/RIT-SE-Senior-Project/nginx/senior-project.conf /etc/nginx/conf.d/
+echo "Moving tls_params config to /etc/nginx/conf.d/"
+sudo cp /home/website/website/RIT-SE-Senior-Project/nginx/tls_params /etc/nginx/
 echo "Installing dependencies"
-cd ./server
+cd /home/website/website/RIT-SE-Senior-Project/server
 npm install
-cd ../ui
+cd /home/website/website/RIT-SE-Senior-Project/ui
 npm install
 echo "Building react app"
 sudo npm run-script build
-cd ..
 echo "Stopping nginx"
 sudo systemctl stop nginx
 echo "Starting nginx"
@@ -21,4 +20,4 @@ sudo systemctl start nginx
 echo "Stopping pm2"
 pm2 stop main
 echo "Starting pm2"
-pm2 start ./server/main.js
+pm2 start /home/website/website/RIT-SE-Senior-Project/server/main.js
