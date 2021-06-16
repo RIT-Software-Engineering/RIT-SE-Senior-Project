adfbd6c2b40936606e60d6b3a36f73ef1c906bec
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2021-06-16 15:18:26 -0400

    Add table to show coach info in coaches tab

--- ui/src/components/shared/SemesterCoaches.js
+++ ui/src/components/shared/SemesterCoaches.js
@@ -1,7 +1,36 @@
 import React from 'react'
-import { Accordion } from 'semantic-ui-react';
+import { Accordion, Table, TableHeader, TableHeaderCell, TableBody, TableRow, TableCell } from 'semantic-ui-react';
+import { USERTYPES } from '../util/constants';
+import ViewProjectMembers from './ViewProjectMembers';
 
 export default function SemesterCoaches({coaches}) {
+
+    const content = (coach) => {
+        return <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHeaderCell>Project</TableHeaderCell>
+                    <TableHeaderCell>Organization</TableHeaderCell>
+                    <TableHeaderCell>Status</TableHeaderCell>
+                    <TableHeaderCell>Coaches</TableHeaderCell>
+                    <TableHeaderCell>Students</TableHeaderCell>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {coach.projects?.map(project => {
+                    return <TableRow key={project.project_id}>
+                        <TableCell>{project.title}</TableCell>
+                        <TableCell>{project.organization}</TableCell>
+                        <TableCell>{project.status}</TableCell>
+                        <TableCell><ViewProjectMembers projectId={project.project_id} projectName={project.title} type={USERTYPES.COACH} /></TableCell>
+                        <TableCell><ViewProjectMembers projectId={project.project_id} projectName={project.title} type={USERTYPES.STUDENT} /></TableCell>
+                    </TableRow>
+                })}
+            </TableBody>
+        </Table>
+
+
+    }
 
     return (
         <>
@@ -13,9 +42,7 @@ export default function SemesterCoaches({coaches}) {
                         key: coach.system_id,
                         title: `${coach.fname} ${coach.lname}`,
                         content: {
-                            content: coach.projects?.map(project => {
-                                return <p>{project.title}</p>
-                            })
+                            content: content(coach)
                         }
                     }
                 })}
