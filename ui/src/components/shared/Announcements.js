c041cf96edee2971c0fc989242104336a71d607f
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2021-07-07 00:09:37 -0400

    Fix warning

--- ui/src/components/shared/Announcements.js
+++ ui/src/components/shared/Announcements.js
@@ -5,7 +5,7 @@ export default function Announcements(props) {
 
     return <div className="announcement-container">
         {props.announcements.map(announcement => {
-            return <div className="announcement">
+            return <div className="announcement" key={announcement.action_id}>
                 <h4>{announcement.action_target === ACTION_TARGETS.coach_announcement && "Coach:"} {announcement.action_title}</h4>
                 <br />
                 <div dangerouslySetInnerHTML={{ __html: announcement.page_html }} />
