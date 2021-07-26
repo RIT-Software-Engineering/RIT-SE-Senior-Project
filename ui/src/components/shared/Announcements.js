5f827c537375650e06004361af160a87cd8a3268
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2021-07-26 14:59:40 -0400

    Fix announcement end dates just displaying today
    Announcement end dates now display actual end date

--- ui/src/components/shared/Announcements.js
+++ ui/src/components/shared/Announcements.js
@@ -9,7 +9,7 @@ export default function Announcements(props) {
             return <div className="announcement" key={announcement.action_id}>
                 <div className="announcement-header">
                     <h4>{announcement.action_target === ACTION_TARGETS.coach_announcement && `Coach (${props.semesterName}):`} {announcement.action_title}</h4>
-                    <h4 className="secondary" >Ends {formatDate(props.end_date)}</h4>
+                    <h4 className="secondary" >Ends {formatDate(announcement.due_date)}</h4>
                 </div>
                 <div dangerouslySetInnerHTML={{ __html: announcement.page_html }} />
             </div>
