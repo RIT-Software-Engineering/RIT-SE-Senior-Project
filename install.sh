5c8fb8b6815d3ecee7a827114fecc9f0d5fe4500
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2021-08-20 02:00:47 -0400

    spell status correctly

--- install.sh
+++ install.sh
@@ -22,6 +22,6 @@ echo "Stopping pm2"
 # be updated in pm2 without a full restart of pm2.
 sudo pm2 kill
 # Start pm2 daemon before starting main.js beacuse otherweise pm2 has a hissy fit and doesn't want to start main properly.
-sudo pm2 staus  # Random pm2 command just to start pm2 daemon
+sudo pm2 status  # Random pm2 command just to start pm2 daemon
 echo "Starting pm2"
 sudo pm2 start /home/website/RIT-SE-Senior-Project/server/main.js
