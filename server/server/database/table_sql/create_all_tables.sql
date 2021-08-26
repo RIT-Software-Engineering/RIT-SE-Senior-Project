bf0627f4d21ea4073e8ad5f2c5ce35cfa2d78084
Max Hoecker <hoecker.max@gmail.com>
2021-08-26 12:19:28 -0400

    forgot a table, whoops...

--- server/server/database/table_sql/create_all_tables.sql
+++ server/server/database/table_sql/create_all_tables.sql
@@ -1,6 +1,7 @@
 .read table_sql/action_log.sql
 .read table_sql/actions.sql
 .read table_sql/archive.sql
+.read table_sql/project_coaches.sql
 .read table_sql/projects.sql
 .read table_sql/semester_group.sql
 .read table_sql/sponsors.sql
--- server/server/database/test_data/fill_test_data.sql
+++ server/server/database/test_data/fill_test_data.sql
@@ -1,6 +1,7 @@
 .read test_data/action_log_dummy.sql
 .read test_data/actions_dummy.sql
 .read test_data/legacy_projects_dummy.sql
+.read test_data/project_coaches_dummy.sql
 .read test_data/project_dummy.sql
 .read test_data/semester_dummy.sql
 .read test_data/sponsor_dummy.sql
