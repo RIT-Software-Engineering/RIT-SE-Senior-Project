d85cd95c1cc49206a926be33058b015377f254be
Max Hoecker <hoecker.max@gmail.com>
2021-08-26 12:08:15 -0400

    added sql files to make creating a test db easier

---
+++ server/server/database/table_sql/create_all_tables.sql
@@ -0,0 +1,7 @@
+.read table_sql/action_log.sql
+.read table_sql/actions.sql
+.read table_sql/archive.sql
+.read table_sql/projects.sql
+.read table_sql/semester_group.sql
+.read table_sql/sponsors.sql
+.read table_sql/users.sql
