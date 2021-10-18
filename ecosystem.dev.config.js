e24a073396471a73c608f82501b15d8c1e07eb76
Max Hoecker <hoecker.max@gmail.com>
2021-10-18 13:46:19 -0400

    maybe it has a hard time finding what port to do stuff on

--- ecosystem.dev.config.js
+++ ecosystem.dev.config.js
@@ -5,7 +5,7 @@ module.exports = {
             script : "./server/main.js",
             env: {
                 NODE_ENV: "development",
-                BASE_URL: "https://seniorproject.se.rit.edu/test",
+                BASE_URL: "https://seniorproject.se.rit.edu",
                 PORT: "3002"
             },
         },
