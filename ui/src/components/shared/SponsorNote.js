137ed56e5c1666052ed415ea738c49541790c134
mjh9131 <mjh9131@g.rit.edu>
2021-12-06 12:10:18 -0500

    sponsor notes can be edited now

--- ui/src/components/shared/SponsorNote.js
+++ ui/src/components/shared/SponsorNote.js
@@ -1,5 +1,7 @@
 import {Button, Modal, Segment} from "semantic-ui-react";
 import React from "react";
+import DatabaseTableEditor from "./DatabaseTableEditor";
+import {config} from "../util/constants";
 
 export default function SponsorNote(props){
 
@@ -28,15 +30,63 @@ export default function SponsorNote(props){
             }]}
         />
 
-    let content = <div>
+    let initialState = {
+        sponsor_id: props.note.sponsor_id,
+        note_content: props.note.note_content
+    };
+
+    let submissionModalMessages = {
+        SUCCESS: "The sponsor note has been updated.",
+        FAIL: "We were unable to update the sponsor note.",
+    };
+
+    let submitRoute = config.url.API_POST_CREATE_SPONSOR_NOTE;
+
+    let formFieldArray = [
+        {
+            type: "textArea",
+            label: "Sponsor Note Content",
+            placeHolder: "Sponsor Note Content",
+            name: "note_content",
+            disabled: false
+        }
+    ]
+
+    let noteEditTrigger = <Button icon='edit' floated='right'/>
+
+    let editNoteComponent = (
+        <Segment basic float={'right'}>
+            <DatabaseTableEditor
+                initialState={initialState}
+                submissionModalMessages={submissionModalMessages}
+                submitRoute={submitRoute}
+                formFieldArray={formFieldArray}
+                header={"Edit Sponsor Note"}
+                trigger={noteEditTrigger}
+                preSubmit={(data) => {
+                    data.sponsor_id = props.note.sponsor;
+                    data.previous_note = props.note.previous_note;
+                    return data;
+                }}
+                callback={props.callback}
+            />
+        </Segment>
+    );
+
+    let content = <Segment float={'left'} basic>
         {props.note.note_content}
         <br/>
         &nbsp;&nbsp;&nbsp;&nbsp; {props.note.creation_date} {props.note.author} {revisionsModal}
-    </div>
+    </Segment>
+
+    if(props.noEdit){
+        editNoteComponent = <></>
+    }
 
     return(
-        <Segment>
+        <Segment.Group horizontal>
             {content}
-        </Segment>
+            {editNoteComponent}
+        </Segment.Group>
     )
 }
