29ba6f5942d5f01796e3230351c2c109ba454ebb
Annie Tiet <anniedtiet@gmail.com>
2023-02-21 19:09:06 -0500

    update UniqueProjectPage.js to replace carousel with items

--- ui/src/components/pages/UniqueProjectPage.js
+++ ui/src/components/pages/UniqueProjectPage.js
@@ -1,20 +1,20 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from 'react-router-dom';
-import ItemsCarousel from "react-items-carousel";
 import {Button, Modal, Icon} from "semantic-ui-react";
 import {config} from "../util/functions/constants";
 import ErrorPage from "../pages/ErrorPage";
 import {SecureFetch} from "../util/functions/secureFetch";
 
-const basePosterURL =`${config.url.API_GET_ARCHIVE_POSTER}?fileName=`;
+const basePosterURL = `${config.url.API_GET_ARCHIVE_POSTER}?fileName=`;
+const baseVideoURL = `${config.url.API_GET_ARCHIVE_VIDEO}?fileName=`;
+const baseImageURL = `${config.url.API_GET_ARCHIVE_IMAGE}?fileName=`;
 const baseProjectURL = `${config.url.BASE_URL}/projects/`;
 
 function UniqueProjectPage({projectData}) {
     const [project, setProject] = useState(projectData);
     const { url_slug } = useParams();
+    const [posterOpen, setPosterOpen] = useState(false);
     const [imageOpen, setImageOpen] = useState(false);
-    const [activeItemIndex, setActiveItemIndex] = useState(0);
-    const chevronWidth = 40;
     const nodeRef = React.useRef(null);
 
     useEffect(() => {
@@ -40,123 +40,97 @@ function UniqueProjectPage({projectData}) {
         }
     }, [url_slug, project]);
 
-    /**
-     * Toggle image modal with expanded project details
-     */
-    const toggleImageModal = () => {
-        setImageOpen(!imageOpen);
-    }
-
-    /**
-     * Creates array of carousel content to be rendered
-     */
-    const makeCarouselContent = () => {
-        let carouselContent = [];
-        if(project?.poster_thumb !== null) {
-            carouselContent.push({"type": "image", "content": project?.poster_thumb});
-        }
-        if(project?.poster_full !== null) {
-            carouselContent.push({"type": "image", "content": project?.poster_full});
-        }
-        if(project?.video !== null) {
-            carouselContent.push({"type": "video", "content": project?.video});
-        }
-        return carouselContent;
-    }
-
-    const carouselContent = makeCarouselContent();
-
     return (
         <div>
-        {project === undefined
-                ? <ErrorPage />
+            {project === undefined
+                ? <ErrorPage/>
                 :
                 <div ref={nodeRef}>
-                    <h1 className="ui header" >{project.title} </h1>
-                            { project?.outstanding === 1 &&
-                                    <Icon name="trophy" title={"Outstanding"} size="large" style={{float: "right"}}/>
-                            }
-                            { project?.creative === 1 &&
-                                <Icon name="trophy" title={"Creative"} size="large" style={{float: "right"}}/>
-                            }
+                    <h1 className="ui header">{project.title} </h1>
+                    {project?.outstanding === 1 &&
+                        <Icon name="trophy" title={"Outstanding"} size="large" style={{float: "right"}}/>
+                    }
+                    {project?.creative === 1 &&
+                        <Icon name="trophy" title={"Creative"} size="large" style={{float: "right"}}/>
+                    }
                     {   // display project page link if slug has been defined
                         project.url_slug !== null && <div>
-                        <Icon name="linkify"/> <a href={`${baseProjectURL}${project.url_slug}`}
-                                                  target="_blank" rel="noreferrer">
-                        {`${baseProjectURL}${project.url_slug}`}</a>
-                    </div>
+                            <Icon name="linkify"/> <a href={`${baseProjectURL}${project.url_slug}`}
+                                                      target="_blank" rel="noreferrer">
+                            {`${baseProjectURL}${project.url_slug}`}</a>
+                        </div>
                     }
-                <div className="ui attached stackable padded grid">
-                    <div className="two column row">
-                        <div className="column">
-                            <div className="ui small header">Dates</div><p>{project?.start_date} - {project?.end_date}</p>
-                            <div className="ui small header">Students</div><p>{project?.members}</p>
-                        </div>
-                        <div className="column">
-                            <div className="ui small header">Sponsor</div><p>{project?.sponsor}</p>
-                            <div className="ui small header">Faculty Coach</div><p>{project?.coach}</p>
+                    <div className="ui hidden divider"></div>
+                    <div className="ui relaxed centered grid">
+                        { project?.poster_thumb &&
+                            <img src={`${basePosterURL}${project?.poster_thumb}`} height={250}
+                                 style={{cursor: "zoom-in", padding: "5px" }}
+                                 onClick={() => setPosterOpen(true)}
+                                 alt={project?.title + " Senior Project Thumbnail Poster"}/>
+                        }
+                        <Modal className={"sticky"}
+                               size={'large'}
+                               open={posterOpen}
+                               onClose={() => setPosterOpen(false)}
+                               onOpen={() => setPosterOpen(true)}>
+                            <Modal.Content>
+                                { project?.poster_full === null
+                                    ? <img className="ui fluid image" src={`${basePosterURL}${project?.poster_thumb}`}
+                                         alt={project?.title + " Senior Project Full Size Poster"}/>
+                                    : <img className="ui fluid image" src={`${basePosterURL}${project?.poster_full}`}
+                                           alt={project?.title + " Senior Project Thumbnail Poster"}/>
+                                }
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button onClick={() => setPosterOpen(false)}>Close</Button>
+                            </Modal.Actions>
+                        </Modal>
+                        { project?.video &&
+                            <video controls height={250}>
+                                <source src={`${baseVideoURL}${project?.video}`} type="video/mp4"/>
+                            </video>
+                        }
+                        { project?.image &&
+                             <img src={`${baseImageURL}${project?.image}`} height={250}
+                                  style={{cursor: "zoom-in", padding: "5px" }}
+                                  onClick={() => setImageOpen(true)}
+                                  alt={project?.title + " Senior Project Image"}/>
+                        }
+                        <Modal className={"sticky"}
+                               size={'large'}
+                               open={imageOpen}
+                               onClose={() => setImageOpen(false)}
+                               onOpen={() => setImageOpen(true)}>
+                            <Modal.Content>
+                                <img className="ui fluid image" src={`${baseImageURL}${project?.image}`}
+                                     alt={project?.title + " Senior Project Image"}/>
+                            </Modal.Content>
+                            <Modal.Actions>
+                                <Button onClick={() => setImageOpen(false)}>Close</Button>
+                            </Modal.Actions>
+                        </Modal>
+                    </div>
+                    <div className="ui hidden divider"></div>
+                    <div className="ui attached stackable padded grid">
+                        <div className="two column row">
+                            <div className="column">
+                                <div className="ui small header">Dates</div>
+                                <p>{project?.start_date} - {project?.end_date}</p>
+                                <div className="ui small header">Students</div>
+                                <p>{project?.members}</p>
+                            </div>
+                            <div className="column">
+                                <div className="ui small header">Sponsor</div>
+                                <p>{project?.sponsor}</p>
+                                <div className="ui small header">Faculty Coach</div>
+                                <p>{project?.coach}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
                     <div className="ui hidden divider"></div>
-                <div style={{ padding: `0 ${chevronWidth}px`, textAlign: "center"}}>
-                    <ItemsCarousel
-                        requestToChangeActive={setActiveItemIndex}
-                        activeItemIndex={activeItemIndex}
-                        numberOfCards={1}
-                        gutter={20}
-                        infiniteLoop={false}
-                        leftChevron={
-                            <button className="circular ui icon button">
-                                <i className="angle left icon"></i>
-                            </button>
-                        }
-                        rightChevron={
-                            <button className="circular ui icon button">
-                                <i className="angle right icon"></i>
-                            </button>
-                        }
-                        outsideChevron
-                        chevronWidth={chevronWidth}>
-                        {carouselContent.map((content, idx) => {
-                                return <div key={idx}>
-                                    <div style={{height: 200}} onClick={toggleImageModal} className="ui container">
-                                        {content.type === "image"
-                                            ? <img src={`${basePosterURL}${content.content}`}
-                                                   alt={project?.title + " Senior Project Poster"}/>
-                                            : <video controls>
-                                                <source src={`${basePosterURL}${content.content}`} type="video/mp4"/>
-                                            </video>
-                                        }
-                                    </div>
-                                    {/* Modal with expanded image, opens when carousel content is clicked */}
-                                    <Modal className={"sticky"}  open={imageOpen}
-                                           onClose={() => setImageOpen(false)}
-                                           onOpen={() => setImageOpen(true)}>
-                                        <Modal.Content>
-                                            {content.type === "image"
-                                                ? <img class="ui fluid image" src={`${basePosterURL}${content.content}`}
-                                                       alt={project?.title + " Senior Project Poster"}/>
-                                                : <video controls>
-                                                    <source src={`${basePosterURL}${content.content}`} type="video/mp4"/>
-                                                </video>
-                                            }
-                                        </Modal.Content>
-                                        <Modal.Actions>
-                                            <Button onClick={() => setImageOpen(false)}>
-                                                Close
-                                            </Button>
-                                        </Modal.Actions>
-                                    </Modal>
-                                </div>
-                            }
-                        )
-                        }
-                    </ItemsCarousel>
-                </div>
                     <div className="ui small header">Synopsis</div>
                     <p>{project?.synopsis}</p>
-            </div>}
+                </div>}
         </div>
     )
 }
