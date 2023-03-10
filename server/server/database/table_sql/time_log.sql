5ce1ca6902b95cf995efcc7102bc1b6dde4d8778
npawl <nrp4309@g.rit.edu>
2023-03-09 21:52:42 -0500

    added time log data table and test data to database.

---
+++ server/server/database/table_sql/time_log.sql
@@ -0,0 +1,12 @@
+CREATE TABLE time_log (
+    time_log_id     INTEGER PRIMARY KEY AUTOINCREMENT,
+    submission_datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
+    system_id       TEXT NOT NULL,
+    project         INTEGER NOT NULL,
+    mock_id         TEXT,
+    work_date       TEXT,
+    time_amount     INTEGER,
+    work_comment    TEXT,
+    FOREIGN KEY (system_id) REFERENCES users(system_id),
+    FOREIGN KEY (project) REFERENCES projects(project_id)
+);
---
+++ server/server/database/test_data/time_log_dummy.sql
@@ -0,0 +1,17 @@
+INSERT INTO time_log(
+    system_id,
+    project,
+    mock_id,
+    work_date,
+    time_amount,
+    work_comment
+)
+VALUES
+    ('qrs123', '2021-5-14_RUM5kpFxW_doOsiZpkdri', NULL, '2023-03-06', 3, 'Coded Widget'),
+    ('tuv123', '2021-5-14_RUM5kpFxW_doOsiZpkdri', NULL, '2023-03-05', 2, 'Debugged Feature'),
+    ('abc123', '2021-5-14_da90mGtCgojqWElAItowB', NULL, '2023-03-07', 4, 'Designed Architecture'),
+    ('nop123', '2021-5-14_RUM5kpFxW_doOsiZpkdri', NULL, '2023-03-06', 1, 'Applied Code Standards'),
+    ('def123', '2021-5-14_da90mGtCgojqWElAItowB', NULL, '2023-03-06', 2, 'Documented Progress'),
+    ('tuv123', '2021-5-14_RUM5kpFxW_doOsiZpkdri', NULL, '2023-03-07', 3, 'Resolved Merge Request'),
+    ('nop123', '2021-5-14_RUM5kpFxW_doOsiZpkdri', NULL, '2023-03-05', 2, 'Ate Spaghetti Code')
+;
