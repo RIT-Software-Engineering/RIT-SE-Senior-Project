aab5a1c0317b32e444820a645720a4ecec84f9c0
jltlm <jlam011235@gmail.com>
2024-05-28 01:04:36 -0400

    gantt headers connect to real y/m/d and gantt action bars are placed on chart according to action start/end dates

--- ui/src/components/Tabs/DashboardTab/TimelinesView/Timeline/GanttChart.js
+++ ui/src/components/Tabs/DashboardTab/TimelinesView/Timeline/GanttChart.js
@@ -1,29 +1,62 @@
 import React, { act, createElement } from 'react'
 import { ACTION_STATES } from '../../../../util/functions/constants';
+import { parseDate } from "../../../../util/functions/utils";
 import _ from "lodash";
 import ToolTip from "./ToolTip";
-import {
-    parseDate,
-  } from "../../../../util/functions/utils";
   
-// a copy of UpcomingActions
 export default function GanttChart(props) {
     const sortedActions = _.sortBy(props.actions || [], ["due_date", "start_date", "action_title"]);
+    let ganttCols = []
     let ganttBars = [];
 
+    // not actually the first day, but I didn't feel like sorting the actions again by start date
+    // let firstDay = sortedActions[0]?.start_date;
 
-    let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    let weekNames = ["Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"];
+    // eventually set it to current date (no param). this is for dev
+    let today = new Date("2019/8/27");
 
-    
-    let cols = 7;
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const weekNames = ["Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"];
 
-    // columns - currently only a weekly view
-    let ganttCols = []
+    let cols = 70; // arbitrary for now
+
+    // curr for current ___ in the construction of the chart
+    let currDate = today.getDate();
+    let currMonth = today.getMonth();
+    let currYear = today.getFullYear();
+    let monthLength = daysInMonth(today.getMonth(), today.getFullYear());    
+
+    const monthLabel = <div
+        className="gantt-first-row"
+        style={{'gridColumn' : 1 + ' / span ' + (monthLength - currDate + 1)}}
+        >
+            {monthNames[currMonth]} {currYear}
+        </div>
+    ganttCols.push(monthLabel);
+
+    // columns
     for (let i = 0; i < cols; i++) {
-        ganttCols.push(<div className="gantt-first-row">month</div>);
-        ganttCols.push(<div className="gantt-second-row">{weekNames[i]}</div>);
+        // if new month
+        if (currDate == monthLength + 1) {
+            currDate = 1;
+            currMonth = currMonth + 1;
+            if (currMonth == 13) {
+                currMonth = 1;
+                currYear++;
+            }
+            monthLength = daysInMonth(currMonth, currYear);
+            const monthLabel = <div
+                className="gantt-first-row"
+                style={{'gridColumn' : i + 1 + ' / span ' + monthLength}}
+                >{monthNames[currMonth]} {currYear}</div>
+            ganttCols.push(monthLabel);
+        }
+        ganttCols.push(<div className="gantt-second-row">{currDate}</div>); // date
+        // ganttCols.push(<div className="gantt-second-row">{weekNames[(today.getDay() + i)%7]}</div>); //days of week
+
         // ganttCols.push(<div className="gantt-cols">.</div>);
+
+        currDate++;
     }
 
     // rows
@@ -49,24 +82,17 @@ export default function GanttChart(props) {
                 break;
         }
 
-        // perhaps find way to not use css inline? feels messy
         const gridrow = 3 + idx;
+        const startDate = action?.start_date;
+        const dueDate = action?.due_date;        
+        const barStart = dateDiff(today, startDate) < 1 ? 1 : dateDiff(today, startDate); 
+        const barSpan = dateDiff(today, startDate) < 1 ? dateDiff(startDate, dueDate) + dateDiff(today, startDate) : dateDiff(startDate, dueDate);
 
-        // will want to use span from start to end date
-        // use parseDate(start/duedate) to get the date
-        let startDate = action?.start_date;
-        let dueDate = action?.due_date;
-        
         const ganttRow = <button
-            className="gantt-bars"
-            style={{'gridRow' : gridrow, 'gridColumn' : '4 / span 3'}} //example span, eventually use dates to determine this
-            // className={`action-bar ${color}`}
+            className={`action-bar ${color}`}
+            style={{'gridRow' : gridrow, 'gridColumn' : barStart + ' / span ' + barSpan}}
             key={idx}
-        >
-            {<div className="gantt-action-bar" title={action.action_title}>
-                {action.action_title}
-            </div>}
-        </button>
+            >{action.action_title}</button>
 
         ganttBars.push(
             <ToolTip
@@ -82,7 +108,6 @@ export default function GanttChart(props) {
         )
     })
 
-    // full container
     let container = <div className="gantt-container">{ganttCols}{ganttBars}</div>
 
     // time span currently does nothing
@@ -102,3 +127,14 @@ export default function GanttChart(props) {
         </div>
     );
 }
+
+function daysInMonth (month, year) {
+    return new Date(year, month, 0).getDate();
+}
+
+// magic number 86400000 is milli * sec * min * hr
+// difference in days
+function dateDiff (firstDate, secondDate) {
+    return (parseDate(secondDate) - parseDate(firstDate)) / 86400000;
+}
+
