7bf1be0320597dfbf0f716872f28b165eb7a6524
Aiden Green <70248258+blaqat@users.noreply.github.com>
2024-06-12 19:07:12 -0400

    Created Component - Question Table
    Question Table is a 3d table that lets the student give how they feel about every other student for all the questions

--- ui/src/components/util/components/PeerEvalComponents.js
+++ ui/src/components/util/components/PeerEvalComponents.js
@@ -1,9 +1,76 @@
 import { useState } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import {
-    Grid, GridColumn, GridRow, Header, Radio
+    Grid, GridColumn, GridRow,
+    Header, Radio,
+    Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow,
 } from 'semantic-ui-react';
 
+// TODO: Add Feedback Component - Basically just a header and text area, non required
+
+// TODO: Change props to a single props table
+// TODO: Add propogation of onChange handler// TODO: Make fields required unles specified otherwise in props
+export const QuestionTable = ({ questions, students }) => {
+    const questionRatings = {};
+    questions.forEach(question => questionRatings[question] = {})
+    const [selections, setSelections] = useState(questionRatings);
+
+    const handleRate = (student, question, rating) => {
+        setSelections(prevSelections => ({
+            ...prevSelections,
+            [question]: {
+                ...prevSelections[question],
+                [student]: rating
+            }
+        }));
+    };
+
+    return (
+        <Table basic='very' celled collapsing unstackable >
+            <TableHeader>
+                <TableRow>
+                    <TableHeaderCell />
+                    {questions.map(question => (
+                        <TableHeaderCell style={{ width: '100px', wordWrap: 'break-word', textAlign: 'center', verticalAlign: 'bottom' }} key={question}>
+                            <Header as='h4'> {question} </Header>
+                        </TableHeaderCell>
+                    ))}
+                </TableRow>
+            </TableHeader>
+
+            <TableBody>
+                {
+                    students.map(student => (
+                        <TableRow key={student}>
+                            <TableCell>
+                                <Header as='h4'> {student} </Header>
+                                {/* <Label size='large' basic>{student}</Label> */}
+                            </TableCell>
+                            {
+                                questions.map(question => (
+                                    <TableCell key={question}>
+                                        <Rating
+                                            maxRating={5}
+                                            defaultRating={selections[question][student] || 0}
+                                            clearable
+                                            // icon='star'
+                                            onRate={(_, data) => handleRate(student, question, data.rating)}
+                                        />
+                                    </TableCell>
+                                ))
+                            }
+                        </TableRow>
+
+                    ))
+                }
+
+            </TableBody>
+        </Table>
+    );
+};
+
+// TODO: Change props to a single props table
+// TODO: Add propogation of onChange handler
 export const QuestionMoodRating = ({ question, student_names, satisfactionLevels }) => {
     if (!satisfactionLevels) {
         satisfactionLevels = ['Extremely Dissatisfied', 'Dissatisfied', 'Neutral', 'Satisfied', 'Extremely Satisfied'];
