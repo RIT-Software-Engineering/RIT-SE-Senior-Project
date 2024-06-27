31164cc4e4cc2d4d557148d8989f0f1e4416e1ac
luci <lucille_blain@live.com>
2024-06-26 22:37:32 -0400

    Moved Text Editor to main module

--- ui/src/components/shared/editors/FormBuilder.js
+++ ui/src/components/shared/editors/FormBuilder.js
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from "react";
 import Form from "semantic-ui-react/dist/commonjs/collections/Form";
 import Button from "semantic-ui-react/dist/commonjs/elements/Button";
 import QuestionBuilder from "./QuestionBuilder";
-
+import {Grid, Header, Label} from "semantic-ui-react";
+import ReactCodeMirror from "@uiw/react-codemirror";
+import {eclipse} from "@uiw/codemirror-theme-eclipse";
+import {html} from "@codemirror/lang-html";
 export default function FormBuilder(props) {
     const field = props.field;
     const [formHtml, setFormHtml] = useState("");
@@ -34,23 +37,37 @@ export default function FormBuilder(props) {
 
     return (
         <Form.Field>
+
+            <Grid>
+                <Grid.Row>
             <QuestionBuilder
                 isOpen={editorOpen}
                 onClose={closeEditor}
                 currentCode={formHtml}
                 onChange={handleChange}
             />
-            <Button disabled={false} onClick={openEditor} floated={"right"}>
+                    <Grid.Column width={8}>
+                        <Header as="h4">Page html</Header>
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+            <Button disabled={false}   onClick={openEditor} floated={"right"}>
                 Open Live Editor
             </Button>
-            <Form.TextArea
-                placeholder={field.placeholder}
-                label={field.label}
-                name={field.name}
-                value={formHtml}
-                style={{ minHeight: 200 }}
+                    </Grid.Column>
+
+                </Grid.Row>
+                <Grid.Row>
+            <Grid.Column>
+            <ReactCodeMirror
+                theme={eclipse}
                 onChange={handleChange}
+                value={formHtml}
+               maxWidth={"200"}
+                extensions={[html({autoCloseTags:true})]}
             />
+            </Grid.Column>
+                </Grid.Row>
+            </Grid>
         </Form.Field>
     );
 }
