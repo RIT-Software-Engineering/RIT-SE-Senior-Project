b633f1b9bf4f8f4aafa139a5d76a64dc0745764e
ayden boyko <aydenboyko@gmail.com>
2025-02-17 15:10:42 -0500

    secure fetch error handling changes

--- ui/src/components/util/functions/secureFetch.js
+++ ui/src/components/util/functions/secureFetch.js
@@ -2,10 +2,10 @@ import { config } from "./constants";
 
 export const SecureFetch = async (url, options) => {
     const response = await fetch(url, { credentials: "include", ...options });
-    if (!response.ok) {// If response is not ok
+    if (!response.ok && response.status === 401) {// If response is not ok
         const data = await response.json(); // Parse JSON
         window.location.replace(`/error`); // Redirect to error page
-        console.log("\n ERROR RESPONSE: ", data);
+        console.log("\n ERROR 401 RESPONSE: ", data);
         sessionStorage.setItem( // Save error details in sessionStorage for display on error page
             "errorDetails",
             JSON.stringify({
@@ -14,7 +14,10 @@ export const SecureFetch = async (url, options) => {
             })
           );
         throw new Error(response);
-    } else {
+    } else if (!response.ok && response.status !== 401) { // not an auth error, doesn't warrant redirect
+        console.log("\n ERROR {" + response.status + "} RESPONSE: ", response);
+        return response;
+    }else {
         console.log("\n RESPONSE: ", response);
         return response;
     }
