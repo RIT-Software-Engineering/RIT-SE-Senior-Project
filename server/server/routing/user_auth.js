630a6e4762c8e683ff29e332916910b208b8310e
Ash Roushinko <ashroushinko@gmail.com>
2025-03-03 14:45:35 -0500

    Initial Addition of ViewOnlyAdmin

--- server/server/routing/user_auth.js
+++ server/server/routing/user_auth.js
@@ -88,7 +88,7 @@ const mockUser = (req, res, next) => {
 }
 
 const testIsAdmin = (req) => {
-    return req.user && req.user.type === ROLES.ADMIN;
+    return req.user && req.user.type === ROLES.ADMIN || req.user && req.user.type === ROLES.VIEWONLYADMIN;
 }
 
 const testCanWrite = (req) => {
