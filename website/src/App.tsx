28cad7c865944a613bbf9934e70f273a2da52248
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2025-03-06 23:40:38 -0500

    Install Tailwind + Shadcn

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
