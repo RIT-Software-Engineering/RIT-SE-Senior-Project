6c763d758da18842b6e995258fdb59e902b005c6
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2025-03-08 18:32:32 -0500

    Reset database in separate script + create backup before reset

---
+++ server/reset-database.js
@@ -0,0 +1,14 @@
+const redeployDatabase = require("./db_setup");
+const fs = require("fs");
+const dbConfig = require("./server/database/db_config.js");
+
+console.log("Creating db backup");
+
+const currentTime = new Date().toISOString();
+const dest = `./server/database/${currentTime}-${dbConfig.dbFileName}`;
+
+fs.copyFileSync(`./server/database/${dbConfig.dbFileName}`, dest);
+
+console.log("Backup made:", dest);
+
+redeployDatabase();
