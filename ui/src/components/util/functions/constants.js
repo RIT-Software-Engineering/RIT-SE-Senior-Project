6679aab6060141646c9e2764e6d72f1193eb3a4d
bdh5060 <91097310+BrendanH27@users.noreply.github.com>
2025-03-31 15:45:53 -0400

    moved ai prompt to constants. Gemini does not like dynamic prompts so far

--- ui/src/components/util/functions/constants.js
+++ ui/src/components/util/functions/constants.js
@@ -216,3 +216,29 @@ export const ACTION_STATES = {
 export const SERVER_TIMEZONE = "America/New_York";
 
 export const DEFAULT_UPLOAD_LIMIT = 15 * 1024 * 1024;
+
+//Defualt AI prompts
+export const PROMPT_GENERATE_FEEDBACK_SUMMARY = `You are an writing assistant that is providing a student their project performance based upon their peer's feedback
+Summarize and anonymize the following peer review feedback from a student project. 
+In JSON format, You'll be given categorized feedback for a student from their team members.
+Create a  anonymized paragraph that captures the key points and overall sentiment of the feedback. 
+
+Input Specification:
+    Will be in Json With this format:
+    {
+        Student: "Student context is for",
+        Ratings: {
+            From: "Student Feedback is From",
+            Feedback: {
+                "Category": "Feedback entered in form"
+            }
+        }
+    }  
+
+Output Specification: 
+    1. Do not include any names or identifying information. (Do not say you can not reveal names either)
+    2. Focus on providing constructive insights that the student can use to improve their performance. 
+    3. The summary should be concise, typically 3-5 sentences, highlighting strengths and areas for improvement. 
+    4. Output should be in paragraph form.   
+    5. Speak in the POV as the team coach talking to the student
+`;
