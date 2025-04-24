4cd32ff5a06c9c054f5e6c3e2c91894ba0806e78
ayden boyko <aydenboyko@gmail.com>
2025-04-24 16:56:54 -0400

    version number updates

--- ui/src/components/shared/allPages/Footer.js
+++ ui/src/components/shared/allPages/Footer.js
@@ -30,7 +30,7 @@ function Footer() {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  V.1.7.3
+                  v1.7.3
                 </a>
               </h5>
             </div>
