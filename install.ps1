87a020e9fcf4da2c2aca9e3b003cc73bd0481e8a
Aditya Vikram <adityavikram2016@gmail.com>
2025-06-07 21:30:20 +0530

    added powershell install script with a couple more options

---
+++ install.ps1
@@ -0,0 +1,35 @@
+param(
+    [switch]$SkipUI,
+    [switch]$SkipServer,
+    [switch]$CleanInstall
+)
+
+cp "$PSScriptRoot/server/.env.sample" "$PSScriptRoot/server/.env"
+cp "$PSScriptRoot/ui/.env.sample" "$PSScriptRoot/ui/.env"
+cp "$PSScriptRoot/server/server/config/rit-cert.pem.sample" "$PSScriptRoot/server/server/config/rit-cert.pem"
+cp "$PSScriptRoot/server/server/config/seniorproject-key.pem.sample" "$PSScriptRoot/server/server/config/seniorproject-key.pem"
+
+if ($CleanInstall) {
+    Write-Host "🧹 Cleaning up all installed packages..."
+    Remove-Item -Recurse -Force "$PSScriptRoot/server/node_modules" -ErrorAction SilentlyContinue
+    Remove-Item -Recurse -Force "$PSScriptRoot/ui/node_modules" -ErrorAction SilentlyContinue
+    Remove-Item -Recurse -Force "$PSScriptRoot/node_modules" -ErrorAction SilentlyContinue
+    Remove-Item -Force "$PSScriptRoot/server/package-lock.json" -ErrorAction SilentlyContinue
+    Remove-Item -Force "$PSScriptRoot/ui/package-lock.json" -ErrorAction SilentlyContinue
+}
+
+if (-not $SkipServer) {
+    Write-Host "ℹ️ Installing server packages..."
+    Set-Location "$PSScriptRoot/server"
+    npm install
+} else {
+    Write-Host "⚠️ Skipping server package installation."
+}
+
+if (-not $SkipUI) {
+    Write-Host "ℹ️ Installing UI packages..."
+    Set-Location "$PSScriptRoot/ui"
+    npm install --legacy-peer-deps
+} else {
+    Write-Host "⚠️ Skipping UI package installation."
+}
