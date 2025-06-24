6ebba1c14a56d1df3223cd34099fdbf9fc964630
Ivan Rojas <itr5720@g.rit.edu>
2025-06-24 17:40:58 -0400

    Changed the sorting of time logs from newest to oldest for coaches to match what the students were seeing

--- server/server/routing/db_routes.js
+++ server/server/routing/db_routes.js
@@ -2867,7 +2867,8 @@ module.exports = (db) => {
                     (SELECT group_concat(users.fname || ' ' || users.lname) FROM users WHERE users.system_id = time_log.mock_id) mock_name
                     FROM time_log
                         JOIN projects ON projects.project_id = time_log.project
-                        WHERE time_log.project IN (SELECT project_id FROM project_coaches WHERE coach_id = ?) `;
+                        WHERE time_log.project IN (SELECT project_id FROM project_coaches WHERE coach_id = ?)
+                        ORDER BY time_log.work_date DESC`;
           queryParams = [req.user.system_id];
           getTimeLogCount = `SELECT COUNT(*) FROM time_log WHERE time_log.project IN (SELECT project_id FROM project_coaches WHERE coach_id = ?)`;
           countParams = [req.user.system_id];
@@ -2878,7 +2879,8 @@ module.exports = (db) => {
                 (SELECT group_concat(users.fname || ' ' || users.lname) FROM users WHERE users.system_id = time_log.system_id) name,
                 (SELECT group_concat(users.fname || ' ' || users.lname) FROM users WHERE users.system_id = time_log.mock_id) mock_name
                 FROM time_log
-                    JOIN projects ON projects.project_id = time_log.project `;
+                    JOIN projects ON projects.project_id = time_log.project
+                    ORDER BY time_log.work_date DESC`;
           queryParams = [];
           getTimeLogCount = `SELECT COUNT(*) FROM time_log`;
           break;
