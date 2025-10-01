97b04b63678d2fec4640ea73252c850175805d8f
Devin <111779169+Rtyujklop@users.noreply.github.com>
2025-10-01 10:17:43 -0400

    made ModelWrapper more flexible and integrated it into SubmissionViewerModal.

--- ui/src/components/Tabs/DashboardTab/TimelinesView/Timeline/SubmissionViewerModal.js
+++ ui/src/components/Tabs/DashboardTab/TimelinesView/Timeline/SubmissionViewerModal.js
@@ -1,29 +1,42 @@
 import React from "react";
+import ModalWrapper from "../../../../shared/ModalWrapper";
 import { Modal } from "semantic-ui-react";
 import SubmissionViewerModalContent from "./SubmissionViewerModalContent";
+import Button from "semantic-ui-react/dist/commonjs/elements/Button/Button";
 
 export default function SubmissionViewerModal(props) {
+  const {
+    open,
+    onClose,
+    action,
+    target,
+    semesterName,
+    projectName,
+    noSubmission,
+    trigger,
+  } = props;
   return (
-    <Modal
+    <ModalWrapper
       open={props.open}
       onClose={props.onClose}
       closeOnDimmerClick={false}
       closeOnEscape={false}
       className="sticky"
       trigger={<div>{props.trigger}</div>}
-      header={`Submission for ${props.action.action_title} (${props.target[0]?.toUpperCase()}${props.target?.substring(1)} Action)`}
-      actions={[{ content: "Close", key: 0 }]}
-      content={{
-        content: (
-          <SubmissionViewerModalContent
-            action={props.action}
-            target={props.target}
-            semesterName={props.semesterName}
-            projectName={props.projectName}
-            noSubmission={props.noSubmission}
-          />
-        ),
-      }}
-    />
+      title={`Submission for ${action.action_title} (${target[0]?.toUpperCase()}${target?.substring(1)} Action)`}
+      actions={[
+        <Button key="close" onClick={onClose}>
+          Close
+        </Button>,
+      ]}
+    >
+      <SubmissionViewerModalContent
+        action={action}
+        target={target}
+        semesterName={semesterName}
+        projectName={projectName}
+        noSubmission={noSubmission}
+      />
+    </ModalWrapper>
   );
 }
--- ui/src/components/shared/ModalWrapper.js
+++ ui/src/components/shared/ModalWrapper.js
@@ -7,12 +7,30 @@ import { Modal } from "semantic-ui-react";
 export default function ModalWrapper({
   open,
   onClose,
+  title,
   size = "small",
+  closeOnDimmerClick = false,
+  closeOnEscape = true,
+  closeIcon = true,
+  className = "",
+  trigger,
+  actions,
   children,
 }) {
   return (
-    <Modal open={open} onClose={onClose} size={size} closeIcon>
-      {children}
+    <Modal
+      open={open}
+      onClose={onClose}
+      size={size}
+      closeOnDimmerClick={closeOnDimmerClick}
+      closeOnEscape={closeOnEscape}
+      closeIcon={closeIcon}
+      className={className}
+      trigger={trigger}
+    >
+      {title && <Modal.Header>{title}</Modal.Header>}
+      <Modal.Content>{children}</Modal.Content>
+      {actions && <Modal.Actions>{actions}</Modal.Actions>}
     </Modal>
   );
 }
