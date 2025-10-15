b6b87ca98d855bb5d41fdd1a5ebea3eefe9a3615
Fabi Marrufo <fm8191@g.rit.edu>
2025-10-15 13:28:48 -0400

    Changed type descriptions.

--- ui/src/components/shared/editors/DatabaseTableEditor.js
+++ ui/src/components/shared/editors/DatabaseTableEditor.js
@@ -33,9 +33,9 @@ const TYPE_HELP = {
   individual:
     "Assigns the same action to each student in the semester group, with completion tracking (red/green) for the team as a whole. Individual students can submit actions again even if they’ve previously done so. Only coaches, admins, and the submitting student can see submitted actions. Other team members can see action status and submission time/date (but not the submitted action).",
   team:
-    "Assigns the same action to each team in the semester group.  Completion (green) requires submission by any team member. Student team members can submit actions even if they’ve previously done so or another team member has done so.  Only coaches, admins, and the submitting student’s team can see submitted actions.  All users can see action status and submission time/date..",
+    "Assigns the same action to each team in the semester group.  Completion (green) requires submission by any team member. Student team members can submit actions even if they’ve previously done so or another team member has done so.  Only coaches, admins, and the submitting student’s team can see submitted actions.  All users can see action status and submission time/date.",
   coach:
-    "",
+    "Assigns the same action to each coah. Only coaches, admins, and the submitting student’s team can see submitted actions.  All users can see action status and submission time/date.",
   peer_evaluation:
   "Peer Evaluation — Create an action by entering a clear title, selecting semester/year, setting start and due dates, then building questions (table ratings, mood ratings, feedback, peer feedback) with the question builder, and copy the generated HTML into the page when finished.",
   student_announcement:
