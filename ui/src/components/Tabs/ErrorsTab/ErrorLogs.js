9d3bede013a29be43b36043acc8e9222d8da33e0
atauln <ataulnoor75@gmail.com>
2025-10-19 20:16:48 -0400

    revised to add styling and add expand functionality for stack traces

--- ui/src/components/Tabs/ErrorsTab/ErrorLogs.js
+++ ui/src/components/Tabs/ErrorsTab/ErrorLogs.js
@@ -2,12 +2,20 @@ import React, { useState, useContext, useEffect } from "react";
 import { SecureFetch } from "../../util/functions/secureFetch";
 import { config, USERTYPES } from "../../util/functions/constants";
 import { UserContext } from "../../util/functions/UserContext";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+} from "semantic-ui-react";
 
 const LOGS_PER_PAGE = 20;
 
 const ErrorLogs = () => {
   const [errorLogs, setErrorLogs] = useState([]);
-  const [errorLogCount, setErrorLogCount] = useState(LOGS_PER_PAGE);
+  const [expandedRows, setExpandedRows] = useState({}); // Track expanded rows
   const [activePage, setActivePage] = useState(0);
   const userContext = useContext(UserContext);
 
@@ -16,10 +24,7 @@ const ErrorLogs = () => {
     SecureFetch(apiUrl)
       .then((response) => response.json())
       .then((error_logs) => {
-        console.log(error_logs);
         setErrorLogs(error_logs);
-        setErrorLogCount(error_logs.length);
-        console.log(errorLogs);
       })
       .catch((error) => {
         alert("Failed to get error log data: " + error);
@@ -30,57 +35,121 @@ const ErrorLogs = () => {
     getErrorLogs();
   }, [activePage]);
 
+  const toggleRow = (id) => {
+    setExpandedRows((prev) => ({
+      ...prev,
+      [id]: !prev[id], // Toggle the expanded state for the row
+    }));
+  };
+
   return (
     <div>
-      <h1>Error Logs</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Error ID</th>
-            <th>Date/Time</th>
-            <th>Status Code</th>
-            <th>User Role</th>
-            <th>URL</th>
-            <th>Stack Trace</th>
-          </tr>
-        </thead>
-        <tbody>
+      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>Error Logs</h1>
+      <Table celled striped selectable>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderCell>Error ID</TableHeaderCell>
+            <TableHeaderCell>Date/Time</TableHeaderCell>
+            <TableHeaderCell>Status Code</TableHeaderCell>
+            <TableHeaderCell>User Role</TableHeaderCell>
+            <TableHeaderCell>URL</TableHeaderCell>
+            <TableHeaderCell>Stack Trace</TableHeaderCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
           {errorLogs && errorLogs.length > 0 ? (
             errorLogs.map((log) => (
-              <tr key={log.error_log_id}>
-                <td>{log.error_log_id}</td>
-                <td>{log.error_datetime}</td>
-                <td>{log.status_code}</td>
-                <td>{log.user_role || "N/A"}</td>
-                <td>{log.url}</td>
-                <td>
-                  <pre>{log.stack_trace || "N/A"}</pre>
-                </td>
-              </tr>
+              <TableRow key={log.error_log_id}>
+                <TableCell>{log.error_log_id}</TableCell>
+                <TableCell>{log.error_datetime}</TableCell>
+                <TableCell>{log.status_code}</TableCell>
+                <TableCell>{log.user_role || "N/A"}</TableCell>
+                <TableCell>{log.url}</TableCell>
+                <TableCell>
+                  <div>
+                    {expandedRows[log.error_log_id] ? (
+                      <pre
+                        style={{
+                          whiteSpace: "pre-wrap",
+                          wordWrap: "break-word",
+                        }}
+                      >
+                        {log.stack_trace || "N/A"}
+                      </pre>
+                    ) : (
+                      <span>
+                        {log.stack_trace
+                          ? log.stack_trace.split("\n")[0] // Show only the first line
+                          : "N/A"}
+                      </span>
+                    )}
+                    {log.stack_trace && (
+                      <button
+                        onClick={() => toggleRow(log.error_log_id)}
+                        style={{
+                          marginLeft: "10px",
+                          background: "none",
+                          border: "none",
+                          color: "#2185d0",
+                          cursor: "pointer",
+                          textDecoration: "underline",
+                        }}
+                      >
+                        {expandedRows[log.error_log_id]
+                          ? "Show Less"
+                          : "Show More"}
+                      </button>
+                    )}
+                  </div>
+                </TableCell>
+              </TableRow>
             ))
           ) : (
-            <tr>
-              <td colSpan="6">No error logs available</td>
-            </tr>
+            <TableRow>
+              <TableCell colSpan={6} style={{ textAlign: "center" }}>
+                No error logs available
+              </TableCell>
+            </TableRow>
           )}
-        </tbody>
-      </table>
-      <div>
+        </TableBody>
+      </Table>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+      >
         <button
           onClick={() => setActivePage((prev) => Math.max(prev - 1, 0))}
           disabled={activePage === 0}
+          style={{
+            padding: "10px 20px",
+            marginRight: "10px",
+            backgroundColor: "#2185d0",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
         >
           Previous
         </button>
         <button
           onClick={() =>
             setActivePage((prev) =>
-              prev < Math.ceil(errorLogCount / LOGS_PER_PAGE) - 1
+              prev < Math.ceil(errorLogs.length / LOGS_PER_PAGE) - 1
                 ? prev + 1
                 : prev,
             )
           }
-          disabled={activePage >= Math.ceil(errorLogCount / LOGS_PER_PAGE) - 1}
+          disabled={
+            activePage >= Math.ceil(errorLogs.length / LOGS_PER_PAGE) - 1
+          }
+          style={{
+            padding: "10px 20px",
+            backgroundColor: "#2185d0",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
         >
           Next
         </button>
