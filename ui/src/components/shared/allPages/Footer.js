7fe233779fbe2e66cda8d81d33e06269bba4f831
Andy Meneely <andy.meneely@rit.edu>
2026-07-02 10:17:43 -0400

    bump to 1.8.3 for hotfix

--- ui/src/components/shared/allPages/Footer.js
+++ ui/src/components/shared/allPages/Footer.js
@@ -30,7 +30,7 @@ function Footer() {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  v1.8.2
+                  v1.8.3
                 </a>
               </h5>
             </div>
